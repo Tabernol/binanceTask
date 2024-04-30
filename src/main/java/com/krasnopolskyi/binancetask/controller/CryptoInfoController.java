0d906924649b009ebca608ae291ec9f1efe0bb55
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2024-05-01 02:29:27 +0300

    Reactive controller was created separately

--- src/main/java/com/krasnopolskyi/binancetask/controller/CryptoInfoController.java
+++ src/main/java/com/krasnopolskyi/binancetask/controller/CryptoInfoController.java
@@ -2,7 +2,6 @@ package com.krasnopolskyi.binancetask.controller;
 
 import com.krasnopolskyi.binancetask.exception.CryptoPairNameException;
 import com.krasnopolskyi.binancetask.model.CryptoInfo;
-import com.krasnopolskyi.binancetask.service.CryptoInfoReactiveService;
 import com.krasnopolskyi.binancetask.service.CryptoInfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.HashSet;
 import java.util.Set;
